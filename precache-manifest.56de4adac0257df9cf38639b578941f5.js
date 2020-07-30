be4f68693ba0d9388a665b0c10e5a1a9410701b3
Anubhav Mittra <anubhavmittra19@gmail.com>
2020-07-31 01:54:23 +0530

    Updates

---
+++ precache-manifest.56de4adac0257df9cf38639b578941f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3e8504952613f59f9f565abe6a50b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c3da0279348781e584c",
+    "url": "/monsters-rolodex/static/css/main.48cc4fea.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3da0279348781e584c",
+    "url": "/monsters-rolodex/static/js/main.63c6c501.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
